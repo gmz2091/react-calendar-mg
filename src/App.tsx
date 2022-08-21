9fcd098d370effdd9bf0621ac55e18095e26f9e8
Magdiel <mgomez2091@gmail.com>
2022-08-20 17:47:21 -0700

    Export Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
-import './App.css';
 import React from 'react';
 
-export const Hello: React.FC<{ name: String }> = ({name}) => {
+export const Main: React.FC<{ name: String }> = ({name}) => {
     if (!name) {
       name = 'Mundo';
     }
